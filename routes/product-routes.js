7452f7b066723810facd103f375e531f8c3b1bd7
wreising <wreising@mac.com>
2022-03-27 21:07:50 -0700

    products is working

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -7,12 +7,24 @@ const { Product, Category, Tag, ProductTag } = require('../models')
 router.get('/products', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, Tag]
+  })
+    .then(products => { res.json(products) })
 })
 
 // get one product
 router.get('/products/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where:
+    {
+      id: req.params.id
+    },
+    include: [Category, Tag]
+  })
+    .then(products => { res.json(products) })
 })
 
 // create new product
@@ -91,6 +103,10 @@ router.put('/products/:id', (req, res) => {
 
 router.delete('/products/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({ where: { id: req.params.id } })
+    .then(products => {
+      res.json(products)
+    })
 })
 
 module.exports = router
--- routes/tag-routes.js
+++ routes/tag-routes.js
@@ -6,23 +6,47 @@ const { Tag, Product, ProductTag } = require('../models')
 router.get('/tags', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [Product]
+  })
+    .then(tags => { res.json(tags) })
 })
 
 router.get('/tags/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where:
+    {
+      id: req.params.id
+    },
+    include: [Product]
+  })
+    .then(tags => { res.json(tags) })
 })
 
 router.post('/tags', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then(tags => {
+      res.json(tags)
+    })
 })
 
 router.put('/tags/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, { where: { id: req.params.id } })
+    .then(tags => {
+      res.json(tags)
+    })
 })
 
 router.delete('/tags/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({ where: { id: req.params.id } })
+    .then(tags => {
+      res.json(tags)
+    })
 })
 
 module.exports = router
